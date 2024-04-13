3dd4a980f9f66473b8c2d6f4b6c5b85d81bc6701
Nischal Raut <rautnischal15@gmail.com>
2024-04-13 13:19:59 +0100

    initial changes

--- pages/online-courses/index.js
+++ pages/online-courses/index.js
@@ -1,183 +1,116 @@
-import React, { useState, useEffect } from 'react';
-import Layout from '../../components/Layout/Layout'
-import axios from 'axios';
-import Loading from '../../components/elements/Loading';
-import { useRouter } from 'next/navigation';
-import Subscription from '../../components/Layout/Subscription';
-import Grid2 from '../../components/elements/Grid2';
-import YellowBanner from '../../components/elements/YellowBanner'
-import KickstartButton from '../../components/elements/KickstartButton'
+/* eslint-disable @next/next/no-img-element */
+import React, { useState, useEffect } from "react";
+import Layout from "../../components/Layout/Layout";
+import Subscription from "../../components/Layout/Subscription";
+import YellowBanner from "../../components/elements/YellowBanner";
+import OnlineCourse from "../../components/elements/OnlineCourse";
+import dynamic from "next/dynamic";
+import OnlineCourseVideo from '../../components/elements/OnlineCourseVideo';
+
+function useLockBodyScroll(open) {
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = "hidden";
+    }
+    return () => {
+      document.body.style.overflow = "visible";
+    };
+  }, [open]);
+}
+const OnlineCourseOne = dynamic(
+  () => import("../../components/sliders/OnlineCourse1"),
+  { ssr: false }
+);
+const OnlineCourseTwo = dynamic(
+  () => import("../../components/sliders/OnlineCourse2"),
+  { ssr: false }
+)
+
+const PopularCourseOne = dynamic(
+  () => import("../../components/elements/PopularCourse1"),
+  { ssr: false }
+);
+const PopularCourseTwo = dynamic(
+  () => import("../../components/elements/PopuarCourse2"),
+  { ssr: false }
+);
 
 export default function Index() {
-  const [data,setData] = useState({})
-  const [categories,setCategories] = useState({})
-  const [selectedCategory,setSelectedCategory] = useState(0)
-  const [searchValue, setSearchValue] = useState('');
-  const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
 
-
-  useEffect(() => {
-    const getCourses = async () => {
-      try {
-        const response = await axios.get(`/alison-courses/courses`)
-        setData(response.data.data)
-      } catch (error) {
-        console.log("Couldn't retrieve quiz: ", error);
-      }
-    };
-    getCourses();
-  },[])
-
-  useEffect(() => {
-    const getCategories = async () => {
-      try {
-        const response = await axios.get(`/alison-courses/categories`)
-        setCategories(response.data.data)
-      } catch (error) {
-        console.log("Couldn't retrieve quiz: ", error);
-      }
-    };
-    getCategories();
-  },[])
-
-  const handleCategoryFilter = async (id) => {
-    setSelectedCategory(id)
-    if (id === 0) {
-      try {
-        const response = await axios.get(`/alison-courses/categories`)
-        setCategories(response.data.data)
-      } catch (error) {
-        console.log("Couldn't retrieve quiz: ", error);
-      }
-      return
-    }
-    try {
-      const query = {
-        "page": 1,
-        "category_id": id
-      }
-      console.log(query)
-      const response = await axios.get('alison-courses/courses',{
-        params: query
-      })
-      setData(response.data.data)
-    } catch (error) {
-      console.log("Couldn't retrieve course: ", error)
-    }
-  }
-
-
-  // Render nothing if data is not available yet
-  if (Object.keys(data).length === 0 || Object.keys(categories).length === 0) {
-    return (
-      <Layout>
-        <Loading />
-      </Layout>
-    );
-  }
-
-  // handleSubmit
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (!searchValue) {
-
-    }
-    else {
-      dispatch(searchText(searchValue))
-      router.push('/search-courses')
-    }
-  }
-
-  const filteredCategories = categories.filter(({
-    courses_count
-  }) => courses_count > 0)
+  useLockBodyScroll(isOpen);
 
   return (
     <>
-    <Layout>
-      <section className="section-box">
-          <div className=" banner-hero bg-img-calculator">
-              <div className="container d-flex align-items-center">
-                  <div className="row">
-                      <div className="col-lg-12">
-                          <h2 className="mb-20 ml-20">Online Courses</h2>
-                          <p className="color-text-paragraph-2 ml-20">Ipsum in voluptate ipsum reprehenderit.<br/>product development</p>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </section>
-      <section className="grid__area container assessment-container">
-         <div className="row">
-            <div className="col-lg-12 mb-30">
-                <h2>Free Online Courses</h2>
-                <p>Qui adipisicing duis ad fugiat non aliqua labore eiusmod ut et non.</p>
-            </div>
-            <section className='section-box mb-50'>
-               <YellowBanner />
-            </section>
-         </div>
-      </section>
-      <section className="course__area pt-20 pb-90 grey-bg-3">
-        <div className="container">
-
-          <div className="row">
-            <div className="col-xxl-2 col-xl-3 col-lg-3">
-              <div className="course__sidebar">
-                <div className="course__sidebar-widget white-bg">
-                  <div className="course__sidebar-search">
-                    <form onSubmit={handleSubmit}>
-                      <input onChange={(e) => setSearchValue(e.target.value)} type="text"
-                        placeholder="Search" />
-                    </form>
-                  </div>
-                </div>
-                <div className="course__sidebar-widget white-bg">
-                  <div className="course__sidebar-info">
-                    <h3 className="course__sidebar-title">Categories</h3>
-                    <ul>
-                      <li>
-                        <div className="course__sidebar-check mb-10 d-flex align-items-center" onClick={() => handleCategoryFilter(0)}>
-                          <input type="radio" readOnly checked={selectedCategory === 0 ? "checked" : ""} />
-                          <label>All</label>
+      <Layout>
+        <div className="background-random-online-course mb-25">
+          <OnlineCourse />
+          <section className="section-box mt-25 mb-25">
+            <YellowBanner />
+          </section>
+          <section className="section-box mt-50 mb-25">
+        < OnlineCourseVideo />
+        </section>
+          <section className="section-box mt-50 mb-25">
+                        <div className="section-box wow animate__animated animate__fadeIn">
+                            <div className="container">
+                                <div className="text-center">
+                                    <h2 className="section-title mb-10" style={{fontSize:'36px !important'}}>
+                                    Explore our Course Categories
+                                    </h2>
+                                    <p className="font-lg fw-bold color-text-paragraph w-lg-50 mx-auto" style={{color:'#435579'}}>
+                                    New courses added every week
+                                    </p>
+                                </div>
+                                <div className="box-swiper mt-50 grant-category">
+                                    <div>
+                                        <OnlineCourseOne />
+                                        <OnlineCourseTwo />
+                                        <div className="swiper-button-next" style={{marginTop:'-10px'}} />
+                                        <div className="swiper-button-prev" />
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                      </li>
-                      {filteredCategories.map((category) => (
-                        <li>
-                          <div className="course__sidebar-check mb-10 d-flex align-items-center" onClick={() => handleCategoryFilter(category.id)}>
-                            <input type="radio" readOnly checked={selectedCategory === category.id ? "checked" : ""}/>
-                            <label> {category.code.toUpperCase().replaceAll("-"," ")} </label>
+                    </section> 
+
+          <section className="section-box mb-25">
+                        <div className="section-box wow animate__animated animate__fadeIn">
+                            <div className="container">
+                                <div className="text-center">
+                                    <h2 className="section-title mb-10" style={{fontSize:'36px !important'}}>
+                                   Selection of Popular Courses
+                                    </h2>
+                                    <p className="font-lg fw-bold color-text-paragraph w-lg-50 mx-auto" style={{color:'#435579'}}>
+                                   Advance your career, learn in-demand skills
+                                    </p>
+                                </div>
+                                <div className="box-swiper mt-50 grant-category">
+                                    <div>
+                                        <PopularCourseOne />
+                                        <PopularCourseTwo />
+                                        <div className="swiper-button-next" style={{marginTop:'400px !important'}} />
+                                        <div className="swiper-button-prev" style={{marginTop:'400px !important'}}/>
+                                    </div>
+                                  
+                                </div>
+                                  <div className="col d-flex justify-content-center align-items-center mt-35">
+                        <button href="" className="btn btn-default fs-6">
+                          Start Learning
+                          </button>
                           </div>
-                        </li>
-                      ))}
-
-                    </ul>
-
-                  </div>
-                </div>
-
-              </div>
-            </div>
-            <div className="col-xxl-10 col-xl-9 col-lg-9">
-
-              <div className="row">
-                <div className="col-12">
-                  <div className="course__tab-content">
-                    <div className="tab-content" id="courseTabContent">
-                      <div className="tab-pane fade show active" id="grid" role="tabpanel" aria-labelledby="grid-tab">
-                        <div className="row">
-                          <Grid2 data={data} />
+                            </div>
                         </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+                    </section>
+                    <div className="col d-flex justify-content-center align-items-center mt-50">
+                                    <button className="btn btn-sign-up-inverted fs-6 hover-up font-force-lg">
+                                        Subscribe For Full Access
+                                    </button>
+                                </div>
+          <Subscription />
         </div>
-      </section>
-    </Layout>
+      </Layout>
     </>
   );
 }
+                  
